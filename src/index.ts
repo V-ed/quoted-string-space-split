ae43cebc76b7d05275b29a0f0c05286237e61a26
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2020-06-11 10:17:34 -0400

    Expose ParsedValue

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import { splitSpacesExcludeQuotes, splitSpacesExcludeQuotesDetailed } from './quoted-string-space-split';
+import { ParsedValue, splitSpacesExcludeQuotes, splitSpacesExcludeQuotesDetailed } from './quoted-string-space-split';
 
-export { splitSpacesExcludeQuotes, splitSpacesExcludeQuotesDetailed };
+export { splitSpacesExcludeQuotes, splitSpacesExcludeQuotesDetailed, ParsedValue };
 
 export default splitSpacesExcludeQuotes;
